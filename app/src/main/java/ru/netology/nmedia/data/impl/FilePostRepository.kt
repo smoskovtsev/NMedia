21942075f66a1f396141691be3ac1bad26c207be
Sergey Moskovtsev <sk.moskovtsev@gmail.com>
2022-05-25 23:26:03 +0300

    File storage implemented

---
+++ app/src/main/java/ru/netology/nmedia/data/impl/FilePostRepository.kt
@@ -0,0 +1,103 @@
+package ru.netology.nmedia.data.impl
+
+import android.app.Application
+import android.content.Context
+import androidx.core.content.edit
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import ru.netology.nmedia.data.PostRepository
+import ru.netology.nmedia.dto.Post
+import kotlin.properties.Delegates
+
+class FilePostRepository(
+    private val application: Application
+) : PostRepository {
+
+    private val gson = Gson()
+    private val type = TypeToken.getParameterized(List::class.java, Post::class.java).type
+
+    private val prefs = application.getSharedPreferences(
+        "repo", Context.MODE_PRIVATE
+    )
+    private var nextId: Long by Delegates.observable(
+        prefs.getLong(NEXT_ID_PREFS_KEY, 0L)
+    ) { _, _, newValue ->
+        prefs.edit { putLong(NEXT_ID_PREFS_KEY, newValue)}
+
+    }
+
+    private var posts
+        get() = checkNotNull(data.value) {
+            "Data value should not be null"
+        }
+        set(value) {
+            application.openFileOutput(FILE_NAME, Context.MODE_PRIVATE).bufferedWriter().use {
+                it.write(gson.toJson(value))
+            }
+            data.value = value
+        }
+
+    override val data: MutableLiveData<List<Post>>
+
+    init {
+        val postsFile = application.filesDir.resolve(FILE_NAME)
+        val posts: List<Post> = if (postsFile.exists()) {
+            val inputStream = application.openFileInput(FILE_NAME)
+            val reader = inputStream.bufferedReader()
+            reader.use { gson.fromJson(it, type) }
+        } else emptyList()
+        data = MutableLiveData(posts)
+    }
+
+    override fun like(postId: Long) {
+        posts = posts.map {
+            if (it.id != postId) it
+            else it.copy(
+                likedByMe = !it.likedByMe,
+                likes = when (it.likedByMe) {
+                    false -> it.likes + 1
+                    true -> it.likes - 1
+                }
+            )
+        }
+    }
+
+    override fun share(postId: Long) {
+        posts = posts.map {
+            if (it.id != postId) it
+            else it.copy(shared = true, shares = it.shares + 1)
+        }
+    }
+
+    override fun delete(postId: Long) {
+        posts = posts.filterNot { it.id == postId} //or .filter {it.id != postId}
+    }
+
+    override fun addUpdatePost(post: Post) {
+        if (post.id == PostRepository.NEW_POST_ID) insert(post) else update(post)
+    }
+
+    private fun insert(post: Post) {
+        posts = listOf(
+            post.copy(id = ++nextId)
+        ) + posts
+    }
+
+    private fun update(post: Post) {
+        posts = posts.map {
+            if (it.id == post.id) post else it
+        }
+    }
+
+    private companion object {
+        //const val GENERATED_POSTS_AMOUNT = 1000
+        const val POSTS_PREFS_KEY = "posts"
+        const val NEXT_ID_PREFS_KEY = "nextId"
+        const val FILE_NAME = "posts.json"
+    }
+}
--- app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
@@ -1,16 +1,23 @@
 package ru.netology.nmedia.viewModel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.adapter.PostInteractionListener
 import ru.netology.nmedia.data.PostRepository
+import ru.netology.nmedia.data.impl.FilePostRepository
 import ru.netology.nmedia.data.impl.InMemoryPostRepository
+import ru.netology.nmedia.data.impl.SharedPrefsPostRepository
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.util.SingleLiveEvent
 
-class PostViewModel : ViewModel(), PostInteractionListener {
+class PostViewModel(
+    application: Application
+) : AndroidViewModel(application), PostInteractionListener {
 
-    private val repository: PostRepository = InMemoryPostRepository()
+    private val repository: PostRepository =
+        FilePostRepository(application)
 
     val data get() = repository.data //or val data by repository :: data
 
